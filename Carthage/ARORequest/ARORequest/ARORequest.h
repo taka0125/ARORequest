4a9e440fe3193070b8e64ad2a0de1dc4b44a0f68
🦊Tomoya Hirano <tomoya.hirano@dena.jp>
2017-11-29 21:55:56 +0900

    support carthage

---
+++ Carthage/ARORequest/ARORequest/ARORequest.h
@@ -0,0 +1,19 @@
+//
+//  ARORequest.h
+//  ARORequest
+//
+//  Created by Tomoya Hirano on 2017/11/29.
+//  Copyright © 2017年 Tomoya Hirano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ARORequest.
+FOUNDATION_EXPORT double ARORequestVersionNumber;
+
+//! Project version string for ARORequest.
+FOUNDATION_EXPORT const unsigned char ARORequestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ARORequest/PublicHeader.h>
+
+
